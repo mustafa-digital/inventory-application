0e647b1a19df51c76f27bdaee9189937913dc998
Mustafa <124535608+MUChappy@users.noreply.github.com>
2024-08-20 11:11:53 -0400

    Add Validation to createController for User Input Item name and inventory is now validated To do: Add delete and update functions for items

--- controllers/createController.js
+++ controllers/createController.js
@@ -1,5 +1,16 @@
 /* createController.js */
 const { getCategories, getBrands, insertItem } = require("../db/queries");
+const { body, validationResult } = require("express-validator");
+
+const validateItem = [
+  body("itemName")
+    .trim()
+    .isAlpha()
+    .withMessage("Item Name must only contain alphanumeric characters.")
+    .isLength({ min: 3, max: 25 })
+    .withMessage("Item Name must be between 3 to 25 characters."),
+  body("inventory"),
+];
 
 const createController = {
   get: async (req, res) => {
@@ -13,11 +24,28 @@ const createController = {
       brands: brands,
     });
   },
-  post: async (req, res) => {
-    const { itemName, category, brand, inventory } = req.body;
-    insertItem({ itemName, category, brand, inventory });
-    res.redirect("/");
-  },
+  post: [
+    validateItem,
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        const [categories, brands] = await Promise.all([
+          getCategories(),
+          getBrands(),
+        ]);
+        return res.status(400).render("create", {
+          title: "Add New Item",
+          categories: categories,
+          brands: brands,
+          errors: errors.array(),
+        });
+      }
+
+      const { itemName, category, brand, inventory } = req.body;
+      insertItem({ itemName, category, brand, inventory });
+      res.redirect("/");
+    },
+  ],
 };
 
 module.exports = createController;
